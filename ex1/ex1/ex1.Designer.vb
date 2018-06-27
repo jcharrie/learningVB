f5738f42617932e5db7aa80175cc211f0ea1cfd5
Jean-Claude Charrier <charrierjeanclaude@yahoo.com>
2018-06-27 02:08:07 -0700

    ex1

---
+++ ex1/ex1/ex1.Designer.vb
@@ -0,0 +1,73 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class ex1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.txtBox = New System.Windows.Forms.TextBox()
+        Me.btnOK = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'txtBox
+        '
+        Me.txtBox.Location = New System.Drawing.Point(12, 12)
+        Me.txtBox.Name = "txtBox"
+        Me.txtBox.Size = New System.Drawing.Size(100, 20)
+        Me.txtBox.TabIndex = 0
+        '
+        'btnOK
+        '
+        Me.btnOK.Location = New System.Drawing.Point(13, 226)
+        Me.btnOK.Name = "btnOK"
+        Me.btnOK.Size = New System.Drawing.Size(75, 23)
+        Me.btnOK.TabIndex = 1
+        Me.btnOK.Text = "&OK"
+        Me.btnOK.UseVisualStyleBackColor = True
+        '
+        'btnExit
+        '
+        Me.btnExit.Location = New System.Drawing.Point(197, 226)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(75, 23)
+        Me.btnExit.TabIndex = 2
+        Me.btnExit.Text = "E&xit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'ex1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(284, 261)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnOK)
+        Me.Controls.Add(Me.txtBox)
+        Me.Name = "ex1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents txtBox As TextBox
+    Friend WithEvents btnOK As Button
+    Friend WithEvents btnExit As Button
+End Class
---
+++ ex1/ex1/ex1.vb
@@ -0,0 +1,11 @@
+﻿Public Class ex1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles btnOK.Click
+        MessageBox.Show(
+            txtBox.Text & "!"
+        )
+    End Sub
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Me.Close()
+    End Sub
+End Class
