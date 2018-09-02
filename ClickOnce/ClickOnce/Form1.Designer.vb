e33ed27f0f7967194f508099ac76c76ee94c8572
Jean-Claude Charrier <charrierjeanclaude@yahoo.com>
2018-09-02 02:06:52 -0700

    Ch.15
    clickonce deployment

---
+++ ClickOnce/ClickOnce/Form1.Designer.vb
@@ -0,0 +1,62 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnVersion = New System.Windows.Forms.Button()
+        Me.lblVersion = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'btnVersion
+        '
+        Me.btnVersion.Location = New System.Drawing.Point(65, 84)
+        Me.btnVersion.Name = "btnVersion"
+        Me.btnVersion.Size = New System.Drawing.Size(75, 23)
+        Me.btnVersion.TabIndex = 0
+        Me.btnVersion.Text = "Version"
+        Me.btnVersion.UseVisualStyleBackColor = True
+        '
+        'lblVersion
+        '
+        Me.lblVersion.AutoSize = True
+        Me.lblVersion.Location = New System.Drawing.Point(167, 93)
+        Me.lblVersion.Name = "lblVersion"
+        Me.lblVersion.Size = New System.Drawing.Size(0, 13)
+        Me.lblVersion.TabIndex = 1
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(442, 200)
+        Me.Controls.Add(Me.lblVersion)
+        Me.Controls.Add(Me.btnVersion)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents btnVersion As Button
+    Friend WithEvents lblVersion As Label
+End Class
---
+++ ClickOnce/ClickOnce/Form1.vb
@@ -0,0 +1,5 @@
+﻿Public Class Form1
+    Private Sub btnVersion_Click(sender As Object, e As EventArgs) Handles btnVersion.Click
+        lblVersion.Text = "Version 1.1"
+    End Sub
+End Class
